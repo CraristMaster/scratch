1a49c4133786c03b57ad546c305e19c3313c03b1
Crarist master <craristmaster@yahoo.com>
2025-02-04 08:28:29 +0100

    this is the simple user input in C++
    the application of cpp

---
+++ treaner.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+  int number;
+  string name;
+  cout<<"enter your name:" <<endl;
+  cin>>name;
+  cout<<"hello,"<<name<<endl;
+  cout << "enter a number: "<<endl;
+  cin >> number;
+  cout << "Your number is: " << number;
+  return 0;
+}
